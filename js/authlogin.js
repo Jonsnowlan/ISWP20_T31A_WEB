ba5159b0f9aef64bed0b15d2bc91033ddf293308
Raul Portales <170976@upslp.edu.mx>
2020-05-19 16:08:36 -0500

    verificcion de autorizacion

--- js/authlogin.js
+++ js/authlogin.js
@@ -1,4 +1,11 @@
-// logout
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+        if (user) {
+            console.log('user logged in: ', user);
+        } else {
+            console.log('user logged out');
+        }
+    }) // logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
     e.preventDefault();
